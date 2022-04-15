1f29a658e66fac20520ffab8e6c65b875e5a0375
AJHdata <kcw467@gmail.com>
2022-04-15 14:17:31 +0900

    mnist 결과 페이지 완성

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
-import os
+import os, pickle
+from PIL import Image
+import numpy as np
+
+
 
 app = Flask(__name__)
 
@@ -19,7 +23,23 @@ def mnist() :
         # os.path.dirname(__file__) 는 현재 파일의 경로
         path = os.path.dirname(__file__)+'/upload/' + f.filename
         f.save(path)
-        return '성공'
+
+        # 이미지 수치화, convert 그레이로 스케일링
+        img = Image.open(path).convert('L')
+        # 흰색 검은색 반전시키기
+        img = np.resize(img,(1,784))
+        # img = 255 - img
+
+
+        # 이미지 열기
+        mpath = os.path.dirname(__file__) + '/model1.pickle'
+        with open(mpath, 'rb') as f :
+            model = pickle.load(f)
+        
+        pred = model.predict(img)
+
+        # return '성공!!' + str(pred) # 결과 확인용 return
+        return render_template('mnistresult.html', data=pred)
 
 # __main__ 일 경우에만 run 실행
 # 위에서부터 실행시키기 때문에 다른 이름이 실행시킬 경우, 서버가 죽기때문
